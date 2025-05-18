456dd1af8b45a9da27b4ee609979637ea863fbcc
Ibrahim <ibrahimelothmani98@gmail.com>
2025-05-18 02:01:27 -1200

    Externalizing Configuration

--- src/main/java/com/ibrahim/store/StripePaymentService.java
+++ src/main/java/com/ibrahim/store/StripePaymentService.java
@@ -1,9 +1,13 @@
 package com.ibrahim.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service("stripe")
 public class StripePaymentService implements PaymentService {
+
+    @Value("${stripe.apiUrl}")
+    private String apiUrl;
 
     @Override
     public void processPayment(double amount) {
